fd8af375903d1e899b10faceda2877e8ded09223
ghayas110 <team@logochemist.com>
2022-07-25 23:33:48 +0500

    Ghayas

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import DarkTheme from '../layouts/Dark'
+import Demos from "./home/home1-dark";
+
+export default function Home() {
+  return (
+    <DarkTheme>
+      <Demos />
+     
+    </DarkTheme>
+  )
+}
